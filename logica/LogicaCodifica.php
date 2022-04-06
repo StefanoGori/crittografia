b789e6253fb6d11900e43f00cba19535a075fdc1
StefanoGori <73885945+StefanoGori@users.noreply.github.com>
2022-04-06 11:53:11 +0200

    modifica logica codifica

--- logica/LogicaCodifica.php
+++ logica/LogicaCodifica.php
@@ -1,9 +1,9 @@
 <?php
     $alfabeto=array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
     $k1=$_POST['k1'];
-    $testo_chiaro=array(strtoupper($_POST['testo']));
+    $testo_chiaro=strtoupper($_POST['testo']);
     $lunghezza=count(array_map('trim',$testo_chiaro));
-    $testo_crittografato=array();
+    $testo_crittografato="";
     print_r($_POST);
     echo $lunghezza;
     echo $testo_chiaro;
@@ -15,8 +15,8 @@
             if($testo_chiaro[$i]==$alfabeto[$j]){
                 echo"entro nell'if";
                 echo $testo_chiaro[$i].",".$alfabeto[$j]."<br>";
-                $testo_crittografato[$i] =$alfabeto[($j+$k1)% 26];
-                echo $testo_crittografato[$i];
+                $testo_crittografato.=$alfabeto[($j+$k1)% 26];
+                echo $testo_crittografato;
             }
         }
     }
